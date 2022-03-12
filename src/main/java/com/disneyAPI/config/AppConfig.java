829c3c6913ac46c3480a87b222ba1592bb11861b
Nahuel <walternahuelbarrios@hotmail.com>
2022-03-11 23:42:21 -0300

    add MovieService in appConfig

--- src/main/java/com/disneyAPI/config/AppConfig.java
+++ src/main/java/com/disneyAPI/config/AppConfig.java
@@ -1,7 +1,9 @@
 package com.disneyAPI.config;
 
 import com.disneyAPI.repository.CharacterRepository;
+import com.disneyAPI.repository.MovieRepository;
 import com.disneyAPI.service.CharacterService;
+import com.disneyAPI.service.MovieService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +13,12 @@ public class AppConfig {
     @Bean
     public CharacterService characterService(CharacterRepository characterRepository){
         return new CharacterService(characterRepository);
+    }
+
+    @Bean
+    public MovieService movieService (MovieRepository movieRepository,
+                                      CharacterRepository characterRepository){
+        return new MovieService(movieRepository,characterRepository);
     }
 
 }
