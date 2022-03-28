722a7a2def65e7ae40add44362ed3ae31dde8258
Nahuel <walternahuelbarrios@hotmail.com>
2022-03-28 12:31:37 -0300

    add Movies documentation swagger

--- src/main/java/com/disneyAPI/controller/MovieController.java
+++ src/main/java/com/disneyAPI/controller/MovieController.java
@@ -1,83 +1,56 @@
 package com.disneyAPI.controller;
 
-import com.disneyAPI.domain.Movie;
-import com.disneyAPI.dtos.ErrorDTO;
 import com.disneyAPI.dtos.MovieDTO;
 import com.disneyAPI.dtos.MovieDTOCreation;
 import com.disneyAPI.dtos.MovieDTOList;
 import com.disneyAPI.exceptions.MovieNotFoundException;
-import com.disneyAPI.mapper.MovieMapper;
-import com.disneyAPI.service.MovieService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
-import java.util.stream.Collectors;
 import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-@RestController
-public class MovieController {
+@Tag(name = "Movies", description = "Operations related to Movies")
+public interface MovieController {
 
-    private final MovieService movieService;
-
-    public MovieController(MovieService movieService){
-        this.movieService = movieService;
-    }
-
+    @Operation(
+            summary = "Create new movie",
+            description = "To create a movie, you must access this endpoint.")
     @PostMapping("/movies")
-    public ResponseEntity<MovieDTO> createMovie(@Valid @RequestBody MovieDTOCreation movieDTOCreation){
-        Movie movieDomain = MovieMapper.mapDTOCreationTODomain(movieDTOCreation);
-        MovieDTO movieDTO = MovieMapper.mapDomainToDTO(movieService.createMovie(movieDomain));
-        return ResponseEntity.ok(movieDTO);
-    }
+    @ResponseStatus(HttpStatus.CREATED)
+     MovieDTO createMovie(@Valid @RequestBody MovieDTOCreation movieDTOCreation);
 
+    @Operation(
+            summary = "Get movies list",
+            description = "To get a list of the movies")
     @GetMapping("/movies")
-    public ResponseEntity<List<MovieDTOList>> getAll(){
-        List<MovieDTOList> movieDTOList = movieService.getAll()
-                .stream().map(MovieMapper::mapDomainToDTOList)
-                .collect(Collectors.toList());
-        return ResponseEntity.ok(movieDTOList);
-    }
+    @ResponseStatus(HttpStatus.OK)
+    List<MovieDTOList> getAll();
 
+    @Operation(
+            summary = "Get movie list by Movies id",
+            description = "To get a list of the movies, filtering by movies id, you must access this endpoint.")
     @GetMapping("/movies/{id}")
-    public ResponseEntity<MovieDTO> getDetailsMovie(@PathVariable Integer id) throws MovieNotFoundException {
-        return ResponseEntity.ok(MovieMapper.mapDomainToDTO(movieService.getById(id)));
-    }
+    @ResponseStatus(HttpStatus.OK)
+    MovieDTO getDetailsMovie(@PathVariable Integer id) throws MovieNotFoundException;
 
+    @Operation(
+            summary = "Get movies list by Movies Name",
+            description = "To get a list of the movies, filtering by movies name, you must access this param.")
     @GetMapping(value = "/movies", params = "name")
-    public ResponseEntity<List<MovieDTO>> getNameMovie(@RequestParam String name) {
-        List<MovieDTO> movieDTOList = movieService.getTittle(name)
-                .stream().map(MovieMapper::mapDomainToDTO)
-                .collect(Collectors.toList());
-        return ResponseEntity.ok(movieDTOList);
-    }
+    @ResponseStatus(HttpStatus.OK)
+    List<MovieDTO> getNameMovie(@RequestParam String name);
 
+    @Operation(
+            summary = "Get movies list by Movies order",
+            description = "To get a list of the movies, filtering by movies order, you must access this param.")
     @GetMapping(value = "/movies", params = "order")
-    public ResponseEntity<List<MovieDTO>> getMoviesOrder(@RequestParam(defaultValue = "ASC") String order) throws MovieNotFoundException{
-
-        if (order.equalsIgnoreCase("ASC") || order.equalsIgnoreCase("DESC")){
-            List<MovieDTO> movieDTOList = movieService.getOrder(order)
-                    .stream().map(MovieMapper::mapDomainToDTO)
-                    .collect(Collectors.toList());
-            return ResponseEntity.ok(movieDTOList);
-        }
-        else{
-            return ResponseEntity.badRequest().build();
-        }
-    }
-
-    @ExceptionHandler(MovieNotFoundException.class)
-    public ResponseEntity<ErrorDTO> handleMovieNotFoundExceptions(MovieNotFoundException ex) {
-        ErrorDTO movieNotFound = ErrorDTO.builder()
-                .code(HttpStatus.NOT_FOUND)
-                .message(ex.getMessage()).build();
-        return new ResponseEntity(movieNotFound, HttpStatus.NOT_FOUND);
-    }
-
+    @ResponseStatus(HttpStatus.OK)
+    List<MovieDTO> getMoviesOrder(@RequestParam(defaultValue = "ASC") String order) throws MovieNotFoundException;
 }
